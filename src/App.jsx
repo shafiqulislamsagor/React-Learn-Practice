a258d0c9be75dc361bba2c86d19544bed84f22cf
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-06-29 18:43:55 +0600

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+const App = () => {
+  return (
+    <div>
+      <h3>I am Learning React </h3>
+      <input type="text" placeholder="My name" />
+      <button>Submit</button>
+    </div>
+  );
+};
+
+export default App;
+ 
